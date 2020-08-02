9a84b99845ea8234f4268009f42300313343563b
plengnelq42 <sonsawan.ko@ku.th>
2020-08-02 18:18:08 +0700

    add function to table

---
+++ main.js
@@ -0,0 +1,174 @@
+function show_hide(){
+      var click = document.getElementById("drop-content");
+
+      if(click.style.display === "none"){
+            click.style.display = "block" ;
+      }
+      else{
+            click.style.display = "none" ;
+      }
+}
+
+function show_hide1(){
+      var click = document.getElementById("drop-content1");
+      
+      if(click.style.display === "none"){
+            click.style.display = "block" ;
+      }
+      else{
+            click.style.display = "none" ;
+      }
+}
+
+function show_hide2(){
+      var click = document.getElementById("drop-content2");
+      
+      if(click.style.display === "none"){
+            click.style.display = "block" ;
+      }
+      else{
+            click.style.display = "none" ;
+      }
+}
+
+function show_hide3(){
+      var click = document.getElementById("drop-content3");
+      
+      if(click.style.display === "none"){
+            click.style.display = "block" ;
+      }
+      else{
+            click.style.display = "none" ;
+      }
+}
+function show_hide4(){
+      var click = document.getElementById("drop-content4");
+      
+      if(click.style.display === "none"){
+            click.style.display = "block" ;
+      }
+      else{
+            click.style.display = "none" ;
+      }
+}
+function show_hide5(){
+      var click = document.getElementById("drop-content5");
+      
+      if(click.style.display === "none"){
+            click.style.display = "block" ;
+      }
+      else{
+            click.style.display = "none" ;
+      }
+}
+function show_hide6(){
+      var click = document.getElementById("drop-content6");
+      
+      if(click.style.display === "none"){
+            click.style.display = "block" ;
+      }
+      else{
+            click.style.display = "none" ;
+      }
+}
+function showdata(){
+      var click = document.getElementById("showdata");
+      
+      if(click.style.display === "none"){
+            click.style.display = "block" ;
+      }
+      else{
+            click.style.display = "none" ;
+      }
+}
+function showdata1(){
+      var click = document.getElementById("showdata1");
+      
+      if(click.style.display === "none"){
+            click.style.display = "block" ;
+      }
+      else{
+            click.style.display = "none" ;
+      }
+}
+function showdata2(){
+      var click = document.getElementById("showdata2");
+      
+      if(click.style.display === "none"){
+            click.style.display = "block" ;
+      }
+      else{
+            click.style.display = "none" ;
+      }
+}
+function showdata3(){
+      var click = document.getElementById("showdata3");
+      
+      if(click.style.display === "none"){
+            click.style.display = "block" ;
+      }
+      else{
+            click.style.display = "none" ;
+      }
+}
+function showdata4(){
+      var click = document.getElementById("showdata4");
+      
+      if(click.style.display === "none"){
+            click.style.display = "block" ;
+      }
+      else{
+            click.style.display = "none" ;
+      }
+}
+function showdata5(){
+      var click = document.getElementById("showdata5");
+      
+      if(click.style.display === "none"){
+            click.style.display = "block" ;
+      }
+      else{
+            click.style.display = "none" ;
+      }
+}
+function showdata6(){
+      var click = document.getElementById("showdata6");
+      
+      if(click.style.display === "none"){
+            click.style.display = "block" ;
+      }
+      else{
+            click.style.display = "none" ;
+      }
+}
+function showdata7(){
+      var click = document.getElementById("showdata7");
+      
+      if(click.style.display === "none"){
+            click.style.display = "block" ;
+      }
+      else{
+            click.style.display = "none" ;
+      }
+}
+function showdata8(){
+      var click = document.getElementById("showdata8");
+      
+      if(click.style.display === "none"){
+            click.style.display = "block" ;
+      }
+      else{
+            click.style.display = "none" ;
+      }
+}
+function showdata9(){
+      var click = document.getElementById("showdata9");
+      
+      if(click.style.display === "none"){
+            click.style.display = "block" ;
+      }
+      else{
+            click.style.display = "none" ;
+      }
+}
+
